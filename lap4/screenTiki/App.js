b49072f52bbcfebc0b4e1493aaa7bc36086ba8f2
Tuan2210 <quangtuan496>
2022-09-21 13:31:16 +0700

    up

---
+++ lap4/screenTiki/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <View style={styles.view1}>
+        <Image source={}/>
+      </View> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C4C4C4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
