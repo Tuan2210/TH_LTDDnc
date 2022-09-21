657567b229dad450124714e10d292b7924d68e20
Tuan2210 <quangtuan496>
2022-09-21 22:43:29 +0700

    done 1/4 screenTiki

--- lap4/screenTiki/App.js
+++ lap4/screenTiki/App.js
@@ -2,21 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React, { useState } from "react";
 
-export default function App() {
-  const [countPlus, setCountPlus] = useState(1);
-  const onPressPlus = () => setCountPlus(nextCount => nextCount + 1);
+//npm i react-native-safe-area-context
+//link: https://www.npmjs.com/package/react-native-safe-area-context
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-  const [countMinus, setCountMinus] = useState(countPlus);
-  const onPressMinus = () => setCountMinus(prevCount => prevCount - 1);
+export default function App() {
+  const [count, setCount] = useState(1);
+  const onPressPlus = () => setCount(count => count + 1);
+  const onPressMinus = () => setCount(count => count - 1);
+
+  // function handleQuantityBook(minCount){
+  //   if(count<1) {
+  //     [count, setCount] = useState(1)
+  // }
   
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.view1}>
         <View style={styles.view1_twoColumn}>
-          <Image source={{ uri: 'https://raw.githubusercontent.com/Tuan2210/TH_LTDDnc/master/lap4/images/book.png'}} style={styles.imgBook}/>
-          <View style={styles.view1_txt}>
-            <Text style={styles.txtNguyenHam}>Nguyên hàm tích phân và ứng dụng</Text>
-            <Text style={styles.txtCungCap}>Cung cấp bởi Tiki Trading</Text>
+          <View style={styles.view1_left}>
+            <Image source={{ uri: 'https://raw.githubusercontent.com/Tuan2210/TH_LTDDnc/master/lap4/images/book.png'}} style={styles.imgBook}/>
+            <Text style={styles.txt}>Mã giảm giá đã lưu</Text>
+          </View>
+          <View style={styles.view1_right}>
+            <Text style={styles.txt}>Nguyên hàm tích phân và ứng dụng</Text>
+            <Text style={styles.txt}>Cung cấp bởi Tiki Trading</Text>
             <Text style={styles.txtPriceDiscount}>141.800 đ</Text>
             <Text style={styles.txtPrice}>141.800 đ</Text>
             <View style={styles.quantity} numberOfLines={1}>
@@ -24,20 +34,22 @@ export default function App() {
                 style={styles.btnMinus}
                 onPress={onPressMinus}
               >
-                <Text style={StyleSheet.create({ fontSize: 20 })}>-</Text>
+                <Text style={styles.txt}>-</Text>
               </TouchableOpacity>
-              <Text style={styles.txtQuantity}>{countPlus}</Text>
+              <Text style={styles.txtQuantity}>{count}</Text>
               <TouchableOpacity
                 style={styles.btnPlus}
                 onPress={onPressPlus}
               >
-                <Text style={StyleSheet.create({fontSize: 20})}>+</Text>
+                <Text style={styles.txt}>+</Text>
               </TouchableOpacity>
+              <Text style={styles.txtMuaSau}>Mua sau</Text>
             </View>
+            <Text style={{ fontSize: 20, color: '#134FEC'}}>Xem tại đây</Text>
           </View>
         </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -48,9 +60,8 @@ const styles = StyleSheet.create({
   },
   view1:{
     backgroundColor: '#fff',
-    width: '100%',
-    height: 400,
-    paddingTop: 30
+    height: 300,
+    paddingTop: 20
   },
   view1_twoColumn:{
     flexDirection: 'row',
@@ -58,48 +69,70 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start', // if you want to fill rows left to right
   },
   imgBook:{
-    width: 150,
+    width: 170,
     height: 200,
-    left: 15
   },
-  view1_txt:{
-    width: '65%',
+  view1_left: {
+    height: '98%',
+    width: '37%',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    // borderColor: 'red',
+    // borderStyle: 'solid',
+    // borderWidth: 1
+  },
+  view1_right:{
     // backgroundColor: '#C4C4C4',
-    left: 30
+    width: '58%',
+    height: '98%',
+    left: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    paddingBottom: 5
   },
-  txtNguyenHam:{
+  txt:{
     fontSize: 20,
-  },
-  txtCungCap:{
-    fontSize: 20,
-    top: 20
   },
   txtPriceDiscount:{
     fontSize: 30,
-    top: 30,
     color: 'red',
     fontWeight: 'bold'
   },
   txtPrice:{
     fontSize: 20,
-    top: 30,
     color: '#808080',
     textDecorationLine: 'line-through',
   },
   quantity:{
     flexDirection: 'row',
     display: 'flex',
+    // borderColor: 'red',
+    // borderStyle: 'solid',
+    // borderWidth: 1,
   },
   txtQuantity:{
     fontSize: 20,
-    top: 30
+    left: 15
   },
   btnPlus: {
     alignItems: "center",
     backgroundColor: "#DDDDDD",
+    width: 30,
     paddingLeft: 5,
     paddingRight: 5,
-    top: 30,
-    left: 10
+    left: 35
   },
+  btnMinus: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    width: 30,
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  txtMuaSau: {
+    fontSize: 20,
+    color: '#134FEC',
+    left: '580%'
+  }
 });
