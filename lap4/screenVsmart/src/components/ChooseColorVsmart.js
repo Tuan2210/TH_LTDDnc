89a31d824044f053e14cf713bf5c69ededd7e245
Tuan2210 <quangtuan496>
2022-09-24 11:10:04 +0700

    upload choose color Vsmart

--- lap4/screenVsmart/src/components/ChooseColorVsmart.js
+++ lap4/screenVsmart/src/components/ChooseColorVsmart.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
 
 //link: https://reactnative.dev/docs/navigation
 //npm i @react-navigation/native @react-navigation/native-stack
@@ -10,10 +10,67 @@ import { NavigationContainer } from '@react-navigation/native';
 //     props.navigation.navigate("blueVsmart");
 
 const ChooseColorVsmart = props => {
+    const [imgVsmart, setImgVsmart] = React.useState(require('../components/images/vsmart_live_xanh2.png'));
+    const [txtColorVsmart, settxtColorVsmart] = React.useState('Màu xanh');
+    const [styleTxtColor, setStyleTxtColor] = React.useState(StyleSheet.create({ color: '#234896', fontSize: 20, top: 10 }));
+
+    const onPressSilverVsmart = () => {
+        setImgVsmart(imgVsmart => imgVsmart = (require('../components/images/vs_bac1.png')));
+        settxtColorVsmart(txtColorVsmart => txtColorVsmart = 'Màu bạc');
+        setStyleTxtColor(styleTxtColor => styleTxtColor = StyleSheet.create({ color: 'gray', fontSize: 20, top: 10 }));
+    }
+    const onPressRedVsmart = () => {
+        setImgVsmart(imgVsmart => imgVsmart = (require('../components/images/vs_red_a2.png')));
+        settxtColorVsmart(txtColorVsmart => txtColorVsmart = 'Màu đỏ');
+        setStyleTxtColor(styleTxtColor => styleTxtColor = StyleSheet.create({ color: 'red', fontSize: 20, top: 10 }));
+    }
+    const onPressBlackVsmart = () => {
+        setImgVsmart(imgVsmart => imgVsmart = (require('../components/images/vsmart_black_star1.png')));
+        settxtColorVsmart(txtColorVsmart => txtColorVsmart = 'Màu đen');
+        setStyleTxtColor(styleTxtColor => styleTxtColor = StyleSheet.create({ color: 'black', fontSize: 20, top: 10 }));
+    }
+    const onPressBlueVsmart = () => {
+        setImgVsmart(imgVsmart => imgVsmart = (require('../components/images/vsmart_live_xanh2.png')));
+        settxtColorVsmart(txtColorVsmart => txtColorVsmart = 'Màu xanh');
+        setStyleTxtColor(styleTxtColor => styleTxtColor = StyleSheet.create({ color: '#234896', fontSize: 20, top: 10 }));
+    }
+
     return (
         <View style={styles.container}>
-            {/* <Text>Open up chooseColorVsmart.js to start working on your app!</Text> */}
-            <Button title='Go to Blue Vsmart screen' onPress={() => props.navigation.navigate('BlueVsmart')}/>
+            <View style={styles.viewTwoColumn}>
+                <Image source={imgVsmart} style={styles.imgBlueVsmart}/>
+                <View style={styles.viewTxtHang}>
+                    <Text>Điện Thoại Vsmart Joy 3{'\n'}Hàng chính hãng</Text>
+                    <Text style={styleTxtColor}>{txtColorVsmart}</Text>
+                </View>
+            </View>
+            <View style={styles.viewChooseColor}>
+                <Text style={styles.txtChon}>Chọn một màu bên dưới:</Text>
+                <View style={styles.viewThreeColors}>
+                    <TouchableOpacity 
+                        style={{ width: 80, height: 80, backgroundColor: '#C5F1FB', alignSelf: 'center' }}
+                        onPress={onPressSilverVsmart}
+                    />
+                    <TouchableOpacity 
+                        style={{ width: 80, height: 80, backgroundColor: 'red', alignSelf: 'center' }} 
+                        onPress={onPressRedVsmart}
+                    />
+                    <TouchableOpacity 
+                        style={{ width: 80, height: 80, backgroundColor: 'black', alignSelf: 'center' }}
+                        onPress={onPressBlackVsmart}
+                    />
+                    <TouchableOpacity 
+                        style={{ width: 80, height: 80, backgroundColor: '#234896', alignSelf: 'center' }} 
+                        onPress={onPressBlueVsmart}
+                    />
+                </View>
+                    <TouchableOpacity 
+                        style={styles.btnXong}
+                        onPress={() => props.navigation.navigate('BlueVsmart')}
+                    >
+                        <Text style={styles.txtXong}>XONG</Text>
+                    </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -23,8 +80,51 @@ export default ChooseColorVsmart;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
+    viewTwoColumn: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        backgroundColor: '#fff',
+        padding: 10,
+    },
+    imgBlueVsmart: {
+        width: 120,
+        height: 150,
+    },
+    viewTxtHang: {
+        padding: 10,
+    },
+    viewChooseColor: {
+        flex: 1,
+        backgroundColor: '#C4C4C4',
+        paddingBottom: 15
+    },
+    txtChon: {
+        padding: 15,
+        fontSize: 18
+    },
+    viewThreeColors: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-around",
+        top: '-2%',
+    },
+    btnXong: {
+        width: 330,
+        height: 50,
+        backgroundColor: '#3D79D2',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 10,
+        borderColor: 'red',
+        borderStyle: 'solid',
+        borderWidth: 1,
+    },
+    txtXong: {
+        color: '#fff',
+        fontSize: 20
+    }
 });
