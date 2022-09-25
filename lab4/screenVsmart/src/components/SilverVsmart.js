324df580af7531239391612c80528acd87d7668f
Tuan2210 <quangtuan496>
2022-09-25 11:52:44 +0700

    upload screen silverVsmart

--- lab4/screenVsmart/src/components/SilverVsmart.js
+++ lab4/screenVsmart/src/components/SilverVsmart.js
@@ -1,27 +1,155 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 //link: https://reactnative.dev/docs/navigation
 //npm i @react-navigation/native @react-navigation/native-stack
 //expo i react-native-screens react-native-safe-area-context
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-const BlueVsmart = props => {
-    return (
-        <View style={styles.container}>
-            {/* <Text>Open up BlueVsmart.js to start working on your app!</Text> */}
-            <Button title='Go to choose color Vsmart screen' onPress={() => props.navigation.navigate('ChooseColorVsmart')} />
+//npm i react-native-ratings
+import { Rating } from "react-native-ratings";
+
+const SilverVsmart = (props) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../components/images/vs_bac1.png")}
+        style={styles.imgSilverVsmart}
+      />
+      <Text style={styles.txtHang}>
+        Điện Thoại Vsmart Joy 3 - Hàng chính hãng
+      </Text>
+      <View style={styles.viewTwoColumn}>
+        <Rating style={{ paddingVertical: 10, marginTop: "5%" }} />
+        <Text style={{ marginTop: "10%", paddingLeft: 10 }}>
+          (Xem 828 đánh giá)
+        </Text>
+      </View>
+      <View style={styles.viewTwoColumn}>
+        <Text style={styles.txtPriceDiscount}>1.790.000 đ</Text>
+        <Text style={styles.txtPrice}>1.790.000 đ</Text>
+      </View>
+      <View style={styles.viewTwoColumn}>
+        <Text style={styles.txtRefund}>Ở ĐÂU RẺ HƠN HOÀN TIỀN</Text>
+        <View style={styles.questions}>
+          <Image source={require("./images/questions.png")} />
         </View>
-    );
-}
+      </View>
+      <TouchableOpacity
+        style={styles.btnFourColors}
+        onPress={() => props.navigation.navigate("ChooseColorVsmart")}
+      >
+        <Text style={styles.txtXong}>4 MÀU - CHỌN MÀU</Text>
+        <Text
+          style={{
+            marginLeft: "25%",
+            fontSize: 30,
+            alignItems: "center",
+            marginTop: -5,
+          }}
+        >
+          {">"}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnMua}>
+        <Text style={styles.txtMua}>CHỌN MUA</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default BlueVsmart;
+export default SilverVsmart;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  imgSilverVsmart: {
+    width: 200,
+    height: "40%",
+    alignSelf: "center",
+    top: 10,
+  },
+  txtHang: {
+    alignSelf: "center",
+    top: 20,
+  },
+  viewTwoColumn: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  txtPriceDiscount: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginLeft: "-8.5%",
+  },
+  txtPrice: {
+    color: "#00000094",
+    fontWeight: "bold",
+    textDecorationLine: "line-through",
+    fontSize: 15,
+    padding: 5,
+    paddingLeft: "25%",
+  },
+  txtRefund: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "red",
+    paddingTop: 10,
+    marginLeft: "-40%",
+  },
+  questions: {
+    paddingTop: 10,
+    left: 10,
+  },
+  btnFourColors: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 350,
+    height: 35,
+    alignSelf: "center",
+    top: 20,
+    shadowRadius: 3.84,
+    elevation: 3, //độ lan ra
+  },
+  txtXong: {
+    marginLeft: 90,
+  },
+  btnMua: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#EE0A0A",
+    borderRadius: 10,
+    width: 350,
+    height: 45,
+    alignSelf: "center",
+    top: 80,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3, //độ lan ra
+  },
+  txtMua: {
+    color: "#fff",
+    fontSize: 20,
+  },
 });
