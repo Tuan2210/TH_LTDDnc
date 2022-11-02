13a1409a497da568b629929f6ad6d7e658b3f4e2
Tuan2210 <quangtuan496>
2022-11-02 21:53:59 +0700

    Update BT2.js

--- lab9/src/components/BT2.js
+++ lab9/src/components/BT2.js
@@ -1,10 +1,4 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Animated,
-  Dimensions
-} from "react-native";
+import { StyleSheet, View, Animated, Dimensions } from "react-native";
 import React, { useState } from "react";
 
 const widthScreen = Dimensions.get('window').width;
