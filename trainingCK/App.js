be45f18e3ac65d269c71986155050b189c2d1808
Tuan2210 <quangtuan496>
2022-12-12 19:51:38 +0700

    comments s.th & add npm

--- trainingCK/App.js
+++ trainingCK/App.js
@@ -2,9 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import React from "react";
 
+//npx create-expo-app 'name-prj'
+//npm i --save-dev @types/react-native
+
+//link all icons react-native: https://oblador.github.io/react-native-vector-icons
+//npm i react-native-vector-icons
+
+// link all name animations: https://github.com/oblador/react-native-animatable
+// Link how to code animation: https://blog.bitsrc.io/top-5-animation-libraries-in-react-native-d00ec8ddfc8d
 //npm i react-native-animatable
+
 //npm i axios firebase react-native-safe-area-context
-//npm i @react-navigation/native @react-navigation/native-stack 
+//npm i @react-navigation/native @react-navigation/native-stack @react-navigation/material-top-tabs
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
---
+++ trainingCK/components/tabs/Orders.js
@@ -0,0 +1,37 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  FlatList,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+// import { useRoute } from "@react-navigation/native";
+
+export default function Order() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>List orders</Text>
+      {/* {listCart !== null ? (
+        <FlatList
+          data={listCart}
+          renderItem={Item}
+          keyExtractor={(item) => item.id}
+        />
+      ) : (
+        <Text style={[styles.f_title__title]}>Giỏ hàng rỗng </Text>
+      )} */}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
